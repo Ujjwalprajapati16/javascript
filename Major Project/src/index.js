6803c8af82feca7ec7bc92406658afdf4265b5bc
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-08-07 21:10:13 +0530

    setup completed

--- Major Project/src/index.js
+++ Major Project/src/index.js
@@ -14,7 +14,7 @@ connectDB()
     });
 
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server running on port ${process.env.PORT || 8000}`);
+      console.log(`Server running on port http://localhost:${process.env.PORT || 8000}`);
     });
   })
   .catch((err) => {
---
+++ Major Project/src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLODINARY_CLOUD_KEY,
+  api_key: process.env.CLODINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file has been successfully uploaded
+    console.log("File successfully uploaded", response.url);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); // remove temporary file path saved
+    console.error("Error uploading file to cloudinary", err);
+    return null;
+  }
+};
+
+// const uploadResult = await cloudinary.uploader
+//   .upload(
+//     "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//     {
+//       public_id: "shoes",
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log(uploadResult);
+
+export { uploadOnCloudinary };
